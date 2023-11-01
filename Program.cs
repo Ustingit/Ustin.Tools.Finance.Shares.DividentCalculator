da3b79a5228f47e1d25ed9aacd8d7f1873e6868a
yuryi.ustinovich <ustinhtc@gmail.com>
2023-11-01 14:55:20 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ustin.Tools.Finance.Shares.DividentCalculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
